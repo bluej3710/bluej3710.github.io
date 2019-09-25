f8901486d6197c064bc0d0ad1163c70ef8f40806
Jeremy <bluej3710@gmail.com>
2019-09-25 13:45:22 -0400

    ywebsite

---
+++ Day13.js
@@ -0,0 +1,13 @@
+//Adding calculator js to make calculator to work
+function takeValue(x) {
+	document.getElementById('inputwindow').value += x;
+}
+
+function clearInput(y) {
+	document.getElementById('inputwindow').value = y;
+}
+
+function calculateResult() {
+	var result = eval(document.getElementById('inputwindow').value);
+	document.getElementById('inputwindow').value = result;
+}
